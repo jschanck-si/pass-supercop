761eb0206ced3ad0f0b8f2db3718bba8c87d1620
John M. Schanck <jschanck@securityinnovation.com>
2013-09-23 15:15:33 -0400

    CPASSREF: license headers on scripts

--- cpassref_rader.py
+++ cpassref_rader.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
+#
+# CPASSREF/cpassref_rader.py
+#
+# Copyright 2013 John M. Schanck
+#
+# This file is part of CPASSREF.
+#
+# CPASSREF is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CPASSREF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CPASSREF.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import sys
 import random
